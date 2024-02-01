1f03788e15b05f50bd42164d9ba5a33f6ebd6cf6
Bedh Prasad Sharma <bedh@Bedhs-MacBook-Air.local>
2024-02-01 22:14:15 +0530

    Basic setup done

---
+++ react/src/router.jsx
@@ -0,0 +1,50 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import Users from "./views/Users";
+import Dashboard from "./views/Dashboard";
+import NotFound from "./views/NotFound";
+import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Navigate to={'/users'} />
+    },
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/register',
+                element: <Register />
+            },
+        ]
+    },
+    {
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+
+            {
+                path: '/users',
+                element: <Users />
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            },
+        ]
+    },
+    {
+        path: '*',
+        element: <NotFound />
+    }
+]);
+
+export default router;
